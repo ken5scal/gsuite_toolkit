bbd4f24516f19841df5f3f98bdeb6a258592c65d
ken5scal <kengoscal@gmail.com>
2017-04-22 15:56:24 +0900

    implement interface declared methods

--- services/GroupService.go
+++ services/GroupService.go
@@ -1,11 +1,35 @@
 package services
 
-import "google.golang.org/api/admin/directory/v1"
+import (
+	"google.golang.org/api/admin/directory/v1"
+	"net/http"
+)
 
 // GroupService provides
 // Details are available in a folling link
 // https://developers.google.com/admin-sdk/directory/v1/guides/manage-groups
 type GroupService struct {
-	*admin.Group
-	*admin.Groups
+	*admin.GroupsService
+	*http.Client
+}
+
+// InitGroupService() creates a new instance
+func InitGroupService() (*GroupService) {
+	return &GroupService{}
+}
+
+// SetClient sets client and initialize services
+func (s *GroupService) SetClient(client *http.Client) (error) {
+	srv, err := admin.New(client)
+	if err != nil {
+		return err
+	}
+	s.GroupsService = srv.Groups
+	s.Client = client
+	return nil
+}
+
+// RepeatCallerUntilNoPageToken repeats service call until next token gets empty
+func (s *GroupService) RepeatCallerUntilNoPageToken() error {
+	return nil
 }
